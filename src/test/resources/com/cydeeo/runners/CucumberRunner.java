4c18cafddd311aa4c90add8b41bc9746a22e6141
Ali Camoglu <119692910+alicamoglu@users.noreply.github.com>
2023-09-10 18:30:39 +0200

    for commit warning

--- src/test/resources/com/cydeeo/runners/CucumberRunner.java
+++ src/test/resources/com/cydeeo/runners/CucumberRunner.java
@@ -1,9 +1,20 @@
 package com.cydeeo.runners;
 
 
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
 
-@RunWith
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
 
+        plugin = "html:target/cucumber-reports.html",
+        features = "",
+        glue = "",
+        dryRun = false,
+        tags = "",
+        publish = true
+
+)
 public class CucumberRunner {}
